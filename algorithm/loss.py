09a4592b3061a37ee8f5d7b8bbc5d759d05d481d
BBongjun <bongjun8@naver.com>
2024-04-24 22:04:51 +0900

    수정

--- algorithm/loss.py
+++ algorithm/loss.py
@@ -40,7 +40,7 @@ import torch.nn.functional as F
 #     return loss, loss, pure_ratio, pure_ratio
 
 
-# gradient vanishing 문제때문에 작은 값 더하니까, nan 증상 사라짐.
+# gradient vanishing 문제 때문에 작은 값 더하니까, nan 증상 사라짐.
 def kl_divergence_with_logits(log_probs, targets):
     kl = torch.sum(targets * (torch.log(targets + 1e-10) - log_probs), dim=1)
     if torch.any(torch.isnan(kl)):
@@ -53,24 +53,25 @@ def loss_jocor(y_1, y_2, t, forget_rate, ind, noise_or_not, co_lambda=0.1):
     y_1_probs = F.softmax(y_1, dim=1)
     y_2_probs = F.softmax(y_2, dim=1)
 
-    if torch.any(torch.isnan(y_1_log_probs)) or torch.any(torch.isnan(y_2_log_probs)):
-        print("NaN detected in log_probs")
-    if torch.any(torch.isnan(y_1_probs)) or torch.any(torch.isnan(y_2_probs)):
-        print("NaN detected in probs")
+    # Debugging
+    # if torch.any(torch.isnan(y_1_log_probs)) or torch.any(torch.isnan(y_2_log_probs)):
+    #     print("NaN detected in log_probs")
+    # if torch.any(torch.isnan(y_1_probs)) or torch.any(torch.isnan(y_2_probs)):
+    #     print("NaN detected in probs")
 
     loss_1 = F.cross_entropy(y_1, t, reduction='none')
     loss_2 = F.cross_entropy(y_2, t, reduction='none')
-
-    if torch.any(torch.isnan(loss_1)) or torch.any(torch.isnan(loss_2)):
-        print("NaN detected in Cross Entropy")
+    # Debugging
+    # if torch.any(torch.isnan(loss_1)) or torch.any(torch.isnan(loss_2)):
+    #     print("NaN detected in Cross Entropy")
 
     kl_1_2 = kl_divergence_with_logits(y_1_log_probs, y_2_probs)
     kl_2_1 = kl_divergence_with_logits(y_2_log_probs, y_1_probs)
 
     loss_pick = (loss_1 + loss_2) / 2 + co_lambda * (kl_1_2 + kl_2_1)
-
-    if torch.any(torch.isnan(loss_pick)):
-        print("NaN detected in final loss")
+    # Debugging
+    # if torch.any(torch.isnan(loss_pick)):
+    #     print("NaN detected in final loss")
 
     ind_sorted = torch.argsort(loss_pick)
     num_remember = int((1 - forget_rate) * len(loss_pick))
